cf94bd3f85ffb0da6aefb6f68ee2130f65ff0659
lk321 <orozco6453@gmail.com>
2021-08-03 16:14:28 -0700

    features

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -9,7 +10,7 @@ import {
 } from "./counterSlice";
 import styles from "./Counter.module.css";
 
-export function Counter() {
+const Counter = () => {
     const count = useSelector(selectCount);
     const dispatch = useDispatch();
     const [incrementAmount, setIncrementAmount] = useState("2");
@@ -61,4 +62,6 @@ export function Counter() {
             </div>
         </div>
     );
-}
+};
+
+export default Counter;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Counter } from "../features/counter/Counter";
+import Counter from "../features/counter/Counter";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
